3255bda22cd64e6b600fc34f8525f489925278bd
pinkopaio8 <emanuele.siena98@gmail.com>
2020-01-01 16:13:36 +0100

    Loading screen 2.0
    Loading text and image will appear when you click on "Next" or "Back", in this way there will be time for the thing to search in the server

--- js/start.js
+++ js/start.js
@@ -5,16 +5,17 @@ var counter = -1;
 
 function next(){
   counter++;
+  document.getElementsByClassName('burp')[0].src = "img/loading.png";
   document.getElementsByClassName('text')[0].innerHTML = "<p>Loading...<p>";
   if (House[counter] !== undefined) {
-    document.getElementsByClassName('burp')[0].src = "img/" + House[counter] + ".png";
-    document.getElementsByClassName('counter')[0].innerText = "Carta Numero: "+(counter+1);
     $.ajax({
       url: "texts/" + House[counter] + ".html",
       //type: "POST",
       //datatype: "json",
       success: function(result){
           $(".text").html(result);
+          document.getElementsByClassName('burp')[0].src = "img/" + House[counter] + ".png";
+          document.getElementsByClassName('counter')[0].innerText = "Carta Numero: "+(counter+1);
     }});
   }
   else{
@@ -24,20 +25,20 @@ function next(){
       random = Math.floor(Math.random() * 51);
     }
     House[counter] = random;
-    console.log(random);
-    document.getElementsByClassName('burp')[0].src = "img/" + random + ".png";
-    document.getElementsByClassName('counter')[0].innerText = "Carta Numero: "+(counter+1);
     $.ajax({
       url: "texts/" + random + ".html",
       //type: "POST",
       //datatype: "json",
       success: function(result){
           $(".text").html(result);
+          document.getElementsByClassName('burp')[0].src = "img/" + random + ".png";
+          document.getElementsByClassName('counter')[0].innerText = "Carta Numero: "+(counter+1);
     }});
   }
 }
 
 function back(){
+  document.getElementsByClassName('burp')[0].src = "img/loading.png";
   document.getElementsByClassName('text')[0].innerText = "<p>Loading...<p>";
   if (counter < 0) {
     counter = -1;
@@ -47,14 +48,14 @@ function back(){
   }
   else {
     counter--;
-    document.getElementsByClassName('burp')[0].src = "img/" + House[counter] + ".png";
-    document.getElementsByClassName('counter')[0].innerText = "Carta Numero: "+(counter+1);
     $.ajax({
       url: "texts/" + House[counter] + ".html",
       //type: "POST",
       //datatype: "json",
       success: function(result){
           $(".text").html(result);
+          document.getElementsByClassName('burp')[0].src = "img/" + House[counter] + ".png";
+          document.getElementsByClassName('counter')[0].innerText = "Carta Numero: "+(counter+1);
     }});
   }
 
