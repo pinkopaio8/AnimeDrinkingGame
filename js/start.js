3579e6787c6604de282fd741a9101a593186bbb6
pinkopaio8 <emanuele.siena98@gmail.com>
2020-01-02 02:05:11 +0100

    New Version

--- js/start.js
+++ js/start.js
@@ -7,6 +7,8 @@ function next(){
   counter++;
   document.getElementsByClassName('burp')[0].src = "img/loading.png";
   document.getElementsByClassName('text')[0].innerHTML = "<p>Loading...<p>";
+  document.getElementsByClassName('next')[0].disabled = true;
+  document.getElementsByClassName('return')[0].disabled = true;
   if (House[counter] !== undefined) {
     $.ajax({
       url: "texts/" + House[counter] + ".html",
@@ -16,13 +18,15 @@ function next(){
           $(".text").html(result);
           document.getElementsByClassName('burp')[0].src = "img/" + House[counter] + ".png";
           document.getElementsByClassName('counter')[0].innerText = "Carta Numero: "+(counter+1);
+          document.getElementsByClassName('next')[0].disabled = false;
+          document.getElementsByClassName('return')[0].disabled = false;
     }});
   }
   else{
     before = random;
-    random = Math.floor(Math.random() * 51);
+    random = Math.floor(Math.random() * 100);
     while (random == before) {
-      random = Math.floor(Math.random() * 51);
+      random = Math.floor(Math.random() * 100);
     }
     House[counter] = random;
     $.ajax({
@@ -33,13 +37,13 @@ function next(){
           $(".text").html(result);
           document.getElementsByClassName('burp')[0].src = "img/" + random + ".png";
           document.getElementsByClassName('counter')[0].innerText = "Carta Numero: "+(counter+1);
+          document.getElementsByClassName('next')[0].disabled = false;
+          document.getElementsByClassName('return')[0].disabled = false;
     }});
   }
 }
 
 function back(){
-  document.getElementsByClassName('burp')[0].src = "img/loading.png";
-  document.getElementsByClassName('text')[0].innerText = "<p>Loading...<p>";
   if (counter < 0) {
     counter = -1;
   }
@@ -47,6 +51,10 @@ function back(){
     counter = 0;
   }
   else {
+    document.getElementsByClassName('burp')[0].src = "img/loading.png";
+    document.getElementsByClassName('text')[0].innerHTML = "<p>Loading...<p>";
+    document.getElementsByClassName('next')[0].disabled = true;
+    document.getElementsByClassName('return')[0].disabled = true;
     counter--;
     $.ajax({
       url: "texts/" + House[counter] + ".html",
@@ -56,7 +64,31 @@ function back(){
           $(".text").html(result);
           document.getElementsByClassName('burp')[0].src = "img/" + House[counter] + ".png";
           document.getElementsByClassName('counter')[0].innerText = "Carta Numero: "+(counter+1);
+          document.getElementsByClassName('return')[0].disabled = false;
+          document.getElementsByClassName('next')[0].disabled = false;
     }});
   }
 
+}
+
+function sword(x){
+  x.onclick = null;
+  var para = document.createElement("div");
+  para.setAttribute("class", "sword");
+  para.setAttribute("onclick", "remove(this)");
+  $.ajax({
+    url: "texts/sword.html",
+    success: function(result){
+        para.innerHTML+=result;
+        document.getElementsByClassName('next')[0].disabled = true;
+        document.getElementsByClassName('return')[0].disabled = true;
+  }});
+      document.body.appendChild(para);
+}
+
+function remove(x){
+  document.getElementById('justonce').onclick = function() {sword(this);};
+  x.remove();
+  document.getElementsByClassName('next')[0].disabled = false;
+  document.getElementsByClassName('return')[0].disabled = false;
 }
